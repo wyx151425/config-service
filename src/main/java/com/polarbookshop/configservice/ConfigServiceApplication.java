ca7caac16d3874aa2ec95bb0aaf6f5e3ecff94d7
wyx151425 <wyx151425@163.com>
2025-10-30 03:54:20 +0800

    feat: 构建配置服务器

--- src/main/java/com/polarbookshop/configservice/ConfigServiceApplication.java
+++ src/main/java/com/polarbookshop/configservice/ConfigServiceApplication.java
@@ -4,8 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * @Author: WangZhenqi
+ * @Description: 在 Spring Boot 应用中启用配置服务器
+ * @Date: Created in 2025-10-23 19:41
+ * @Modified By:
+ */
 @SpringBootApplication
-@EnableConfigServer  // 在 Spring Boot 应用中激活配置服务器的实现
+// 在 Spring Boot 应用中激活配置服务器的实现
+@EnableConfigServer
 public class ConfigServiceApplication {
 
     public static void main(String[] args) {
